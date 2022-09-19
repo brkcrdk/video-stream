675049e7001ddb8f20e511eeaca040c63cb572ff
Burak <burak@90pixel.com>
2022-09-19 21:56:12 +0300

    Add express and cors to server app.

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,14 @@
 import { config } from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
 import { createToken } from 'utils';
+
+const app = express();
+const port = 4000;
+
+app.use(cors());
+app.use(express.json());
 
 config();
 const token = createToken({
