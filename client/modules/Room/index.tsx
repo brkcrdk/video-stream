fd45a0c6a7afb3c3b69d26be05eeb679455c116f
Burak <burak@90pixel.com>
2022-10-13 23:38:06 +0300

    Add LivekitRoom to room page.

--- client/modules/Room/index.tsx
+++ client/modules/Room/index.tsx
@@ -1,6 +1,30 @@
 import { useRouter } from 'next/router';
 
+import { DisplayContext, DisplayOptions, LiveKitRoom, useRoom, useParticipant } from '@livekit/react-components';
+import { VideoPresets } from 'livekit-client';
+
 function Room() {
-  return <div>Room is here</div>;
+  const {
+    query: { token },
+  } = useRouter();
+
+  const { room, participants } = useRoom();
+
+  console.log({ participants });
+
+  if (!token) return null;
+
+  return (
+    <LiveKitRoom
+      token={String(token)}
+      url="ws://localhost:7880"
+      roomOptions={{
+        adaptiveStream: true,
+        videoCaptureDefaults: {
+          resolution: VideoPresets.h720.resolution,
+        },
+      }}
+    />
+  );
 }
 export default Room;
