c4079e9db93c936fcc30ab4f55700bf95cbe4c3b
Burak <burak@90pixel.com>
2022-10-18 19:02:24 +0300

    Open video and audio after entering to room.

--- client/modules/Room/index.tsx
+++ client/modules/Room/index.tsx
@@ -1,10 +1,11 @@
 import { useRouter } from 'next/router';
 
 import { DisplayContext, DisplayOptions, LiveKitRoom, useRoom, useParticipant } from '@livekit/react-components';
-import { VideoPresets } from 'livekit-client';
+import { VideoPresets, Room } from 'livekit-client';
 
 import StageRenderer from './StageRenderer';
-function Room() {
+
+function RoomT() {
   const {
     push,
     query: { token },
@@ -14,19 +15,41 @@ function Room() {
 
   if (!token) return null;
 
+  const onConnected = async (room: Room) => {
+    // make it easier to debug
+    (window as any).currentRoom = room;
+
+    // if (isSet(query, 'audioEnabled')) {
+    //   const audioDeviceId = query.get('audioDeviceId');
+    //   if (audioDeviceId && room.options.audioCaptureDefaults) {
+    //     room.options.audioCaptureDefaults.deviceId = audioDeviceId;
+    //   }
+    await room.localParticipant.setMicrophoneEnabled(true);
+    // }
+
+    // if (isSet(query, 'videoEnabled')) {
+    //   const videoDeviceId = query.get('videoDeviceId');
+    //   if (videoDeviceId && room.options.videoCaptureDefaults) {
+    //     room.options.videoCaptureDefaults.deviceId = videoDeviceId;
+    //   }
+    await room.localParticipant.setCameraEnabled(true);
+    // }
+  };
+
   return (
     <LiveKitRoom
       token={String(token)}
       url={String(process.env.NEXT_PUBLIC_WEB_SOCKET_URL)}
       onLeave={() => push('/')}
+      onConnected={onConnected}
       roomOptions={{
         adaptiveStream: true,
         videoCaptureDefaults: {
           resolution: VideoPresets.h720.resolution,
         },
       }}
-      stageRenderer={StageRenderer}
+      // stageRenderer={StageRenderer}
     />
   );
 }
-export default Room;
+export default RoomT;
