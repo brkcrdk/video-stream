fb72b1b2b883cfa0a6a28deaf9a197520bc5ae3d
Burak <burak@90pixel.com>
2022-10-24 14:54:00 +0300

    Add theme and ThemeProvider to application.

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import { ThemeProvider } from 'styled-components';
+import theme from 'theme';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Component {...pageProps} />
       <div id="modal" />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ client/theme/index.ts
@@ -0,0 +1,33 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#1EA4CE',
+  },
+  utils: {
+    lineClamp: (clampCount: number) => `
+    display: -webkit-box;
+    -webkit-line-clamp: ${clampCount};
+    -webkit-box-orient: vertical;
+    overflow: hidden;`,
+  },
+  fontWeights: {
+    thin: 100,
+    regular: 400,
+    medium: 500,
+    light: 300,
+    bold: 700,
+    black: 900,
+  },
+};
+
+export default theme;
+
+export const device = {
+  widescreen: '(max-width: 1440px)',
+  desktop: '(max-width: 1280px)',
+  laptop: '(max-width: 1024px)',
+  tablet: '(max-width: 768px)',
+  phone: '(max-width: 640px)',
+  mini: '(max-width: 425px)',
+};
