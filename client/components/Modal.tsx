ec7875f8a825f952bc36724820f4c972ddd0c5f6
Burak <burak@90pixel.com>
2022-10-25 00:02:27 +0300

    Initialize modal component.

--- client/components/Modal.tsx
+++ client/components/Modal.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, ReactNode } from 'react';
 
 import { createPortal } from 'react-dom';
+import styled from 'styled-components';
 
 interface Props {
   open: boolean;
@@ -18,6 +19,43 @@ function Modal({ open, children }: Props) {
 
   if (!open || !mounted) return null;
 
-  return createPortal(children, document.querySelector('#modal') as HTMLElement);
+  return createPortal(
+    <ModalOverlay>
+      <ModalContainer>{children}</ModalContainer>
+    </ModalOverlay>,
+    document.querySelector('#modal') as HTMLElement
+  );
 }
 export default Modal;
+
+// const ModalOverlay = styled('div', {
+//   position: 'fixed',
+//   inset: 0,
+//   backgroundColor: '$overlay',
+//   display: 'flex',
+//   alignItems: 'center',
+//   justifyContent: 'center',
+//   zIndex: 9,
+//   overflow: 'auto',
+// });
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  inset: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9;
+  overflow: auto;
+  background: ${p => p.theme.colors.grayScale.gray900}40; // Sona eklenen değer alpha değerini ifade eder
+`;
+
+const ModalContainer = styled.div`
+  width: 100%;
+  max-width: 630px;
+  background-color: ${p => p.theme.colors.grayScale.gray900};
+  /* backgroundColor: '$white',
+  padding: '$12',
+  borderRadius: '$3xl',
+  zIndex: 10, */
+`;
